2755ce3d3b8841c1a8d2603ad0c0c6fc07ffd140
joeljdv <joeldiazjdv@gmail.com>
2021-04-23 14:28:08 -0400

    commit tree ae85540718ffd0e601e42d120c1b3799d76efe84

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+
+const MovieReviews = (props) => {
+    return (
+        <div className='review-list'>
+            {props.movies}
+        </div>
+    )
+}
+
+export default MovieReviews
